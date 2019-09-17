260f3f94a478c7ba1ff27d1d542d45129efb61d0
Robin <Robin99g@gmail.com>
2019-09-17 20:43:46 +0200

    Project Upload

---
+++ public/app.js
@@ -0,0 +1,118 @@
+// --- Ml5 Image classification ---
+let mobilenet;
+let input;
+let prediction;
+let inputLink;
+
+// Callback when a result is found
+function gotResult(error, results) {
+    if (error) throw error
+    console.log(results);
+    prediction = results;
+    socket.emit('ml5prediction', prediction);
+}
+
+// Callback for when the model is loaded
+function modelready() {
+    console.log("model is ready");
+    mobilenet.predict(input, gotResult);
+}
+
+// Setup function for ml5 and defining the image to use
+document.getElementById('startButton').addEventListener("click",function(){
+
+});
+function setup() {
+    input = createImg("https://3.imimg.com/data3/NA/AJ/MY-4145714/desktop-computer-500x500.jpg");
+    input.hide();
+}
+
+function start(){
+    mobilenet = ml5.imageClassifier('MobileNet', modelready);
+    const button = document.getElementById('startButton');
+    const startText = document.getElementById('startText');
+    const foundWords = document.getElementById('foundWords');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+    h1.id = 'placeholderHead';
+    p.id = 'placeholderText';
+    h1.innerText = 'Loading...';
+    p.innerText = "The script is working it's magic! This shouldn't take long!";
+
+    inputLink = document.getElementById('inputBox').value.toString();
+    console.log(inputLink);
+    
+    inputBox.remove();
+    button.remove();
+    startText.remove();
+    foundWords.appendChild(h1);
+    foundWords.appendChild(p);
+}
+
+
+// --- Socket IO --- 
+var socket = io();
+
+// Send a message to the server when the html page is loaded
+window.onload = function () {
+    console.log("index.html loaded");
+    socket.emit('clientMsg', "[client] page loaded");
+};
+
+// Check for server messages
+socket.on('serverMsg', function (msg) {
+    console.log(msg);
+})
+
+socket.on('foundImage', function(foundImage){
+    const imageContainer = document.getElementById('foundImage');
+    let imageContent = document.createElement('img');
+    imageContent.src = foundImage;
+    imageContainer.appendChild(imageContent);
+})
+
+socket.on('imagePrediction', function(imagePrediction){
+    const foundWords = document.getElementById('foundWords');
+    let content = document.createElement('p');
+    content.className = 'predictionText';
+    content.innerText = `Image prediction: ${imagePrediction}`;
+    let enter = document.createElement('br');
+
+    foundWords.appendChild(content);
+    foundWords.appendChild(enter);
+})
+
+// Detect when the server sends back the array of associations and append those found words to an empty div
+socket.on('serverWords', function (words) {
+    console.log(words);
+    const foundWords = document.getElementById('foundWords');
+    const placeholder = document.getElementById('placeholderText');
+    const placeholder2 = document.getElementById('placeholderHead');
+
+    placeholder.remove();
+    placeholder2.remove();
+    console.log('Data loaded [loading complete]');
+
+    let paragraph = document.createElement("p");
+    paragraph.className = 'associationsHead';
+    paragraph.innerText = 'I found these associations:'
+    foundWords.appendChild(paragraph);
+
+    words.forEach(word => {
+        let content = document.createTextNode(word);
+        let enter = document.createElement("br");
+        foundWords.appendChild(content);
+        foundWords.appendChild(enter);
+    });
+
+    words.forEach(element => {
+        //debug
+        console.log(element);
+
+        // Create the link for the google engine to search
+        var urlStart = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCf_ZNrvhUMkP6t28I8t-zHwScyJv3u21U&cx=011039081965954186716:44ojrzsn6oh&searchType=image&q=";
+        var fullUrl = urlStart + element;
+        console.log(fullUrl);
+        socket.emit('googleJson', fullUrl);
+    });
+})
---
+++ server.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const fs = require('fs');
+var dot = require('dot-object');
+const fetch = require('node-fetch');
+var publicDir = require('path').join(__dirname,'/public');
+
+// Set the page to index.html
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+
+app.use(express.static(publicDir));
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+// Detect when a user connects to the server
+io.on('connection', function(socket){
+    console.log('User Connected');
+    socket.emit('serverMsg', 'yeet');
+
+    // Detect when the client sends a message to the server
+    socket.on('clientMsg', function(msg){
+        //DEBUG
+        console.log(msg);
+    })
+
+    socket.on('googleJson', function(link){
+        //console.log(link);
+        fetch(link)
+        .then(res => res.json())
+        .then(function(err, json){
+            if (err) throw err
+            //console.log(json);
+            //let googleResults = JSON.parse(json);
+            console.log(json.items[0].image.thumbnailLink);
+            let foundImage = json.items[0].image.thumbnailLink;
+            socket.emit('foundImage', foundImage);
+        })
+    })
+
+    // Detect when the client sends data to the server
+    socket.on('ml5prediction', function(data){
+        // Log the associations found by ml5
+        console.log(data);
+
+        // Put the first found association found in a string
+        let result = data[0].label.toString();
+
+        // Split the string to only get the first association
+        if (result.indexOf(',') != null){
+        result = result.substring(0,result.indexOf(','));
+        };
+        // Debug
+        console.log(result);
+
+        // Search for word associations in the json file
+        fs.readFile('words.json', (err, data) => {
+            if (err) throw err;
+            let words = JSON.parse(data);
+        
+            //console.log(words);
+
+            // Get the path to the associations of the found word in the words object
+            let path = `${result}.associations`;
+            // Convert the path to dot notation with dot-object.js
+            var val = dot.pick(path,words);
+
+            // Log the associations found
+            for (i=0; i<val.length; i++)
+            {
+                console.log(`[${i}]: ${val[i]}`);
+            }
+            // send the found words array to the client
+            socket.emit('imagePrediction', result);
+            socket.emit('serverWords', val);
+
+        });
+    })
+})
+
+
